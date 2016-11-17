f5899e6340d9a1a6bc2d0433969ff7de7fdf315e
Alex Lonsberry <ajl17@case.edu>
2016-11-17 13:47:51 -0500

    Added TEXT file generator (description of saved 1d moving mass data created)
    In src > specAim_1_1_1 > generateInputData.py I added a method
    createDataDescriptionTxtFile() which takes a look at the data I created
    (for 1D moving mass) and then writes out .txt files to describe what is
    in that data to make it easier for someone looking at the data to see
    what it is and how it was made.

--- src/specAim_1_1_1/generateInputData.py
+++ src/specAim_1_1_1/generateInputData.py
@@ -25,7 +25,7 @@ from spikeDataGenerators import analogToSpikes
 # METHODS FOR CREATING 1D MOVING MASS DATA (SMOOTH, DECOMPOSED, SPIKING)
 ########################################################################################################################
 
-def genAndSaveMoving1DMassData(saveName='movingPointMassData/pointMassData000.pkl'):
+def genAndSaveMoving1DMassData(saveName='movingPointMassData/pointMassData000.pkl',Iterations=10):
     """
     DESCRIPTION
     Here we call the methods repeatedly to make instances of the 1D moving mass. Of these n examples, we then put them
@@ -33,15 +33,15 @@ def genAndSaveMoving1DMassData(saveName='movingPointMassData/pointMassData000.pk
     :return: N/A (we save data to a file), the list saved is [listOfTrials, xmin, xmax, vmin, vmax, amin, amax, dt, tmax]
     """
     #How many iterations we want to include
-    Iterations = 10
+    #Iterations = 10 # No more hard coding! This is now an input into the function.
     xmin = 0.0
-    xmax = 5.0
+    xmax = 20.0
     vmin = 1.0
     vmax = 5.0
     amin = 1.0
     amax = 2.0
     dt   = .001
-    tmax = 10.0
+    tmax = 20.0
     dataOut = []
     for i in range(Iterations):
         funcOuts = movingPointMass1D.randomMassMovement(xmin, xmax, vmin, vmax, amin, amax, dt, tmax)
@@ -108,7 +108,7 @@ def decomposeMoving1DMassData(dataFile='movingPointMassData/pointMassData000.pkl
 
     # Now we want to save the decomposed 1D arrays and the Gaussians that made them, and also cite the original
     # data that was used.
-    toSave = [segmentedTrials, gCenters, b, dataFile]
+    toSave = [segmentedTrials, gCenters, b, dataFile, xStart, xStop, nGaus, bInitial, dt]
     outputFile = open(saveName, "wb")
     pickle.dump(toSave, outputFile)
     outputFile.close()
@@ -187,6 +187,7 @@ def decompedToSpikes1DMassData(dataFile='movingPointMassData/pointMassDataDecmp0
     # Fill in spikeGenType: tells us which spike generation optiont to call. The options are "linearIntegral" or
     # "timeRescale"
     spikeGenType = "linearIntegral"
+    analgSingnalScaling = 20.
     # Fill in the
 
     # Load the data back (this is the decomposed version of the 1D moving mass data)
@@ -208,13 +209,13 @@ def decompedToSpikes1DMassData(dataFile='movingPointMassData/pointMassDataDecmp0
     segmentedSpikesList = []
     if spikeGenType=="linearIntegral":
         for i in range(len(segmentedTrialsList)):
-            segmentedSpikesList.append(analogToSpikes.genSpikesLinearly(segmentedTrialsList[i], dt=dt, scaling=20.))
+            segmentedSpikesList.append(analogToSpikes.genSpikesLinearly(segmentedTrialsList[i], dt=dt, scaling=analgSingnalScaling))
 
     # spikeTrains = analogToSpikes.genSpikesWithTimeRescaling(segmentedValues, dt=.001, scaling=10.)
     # spikeTrains = analogToSpikes.genSpikesLinearly(segmentedValues, dt=.001, scaling=20.)
 
     # Save
-    outputList = [segmentedSpikesList, dataFile] # We also want to store the location of the originating decomped data
+    outputList = [segmentedSpikesList, dataFile, spikeGenType, analgSingnalScaling] # We also want to store the location of the originating decomped data
     outputFile = open(saveName, "wb")
     pickle.dump(outputList, outputFile)
     outputFile.close()
@@ -305,6 +306,101 @@ def loadAndPlotDecomp1DMassData(dataFile='movingPointMassData/pointMassDataDecmp
         plt.show()
 
 
+def createDataDescriptionTxtFile(pMassFile=[], pMassDcmpFile=[], pMassDcmpSpkesFile=[]):
+    """
+    DESCRIPTION
+    Here I want to create a .txt file that will describe the data saved in a particular set of files.
+    :param pMassFile:
+    :param pMassDcmpFile:
+    :param pMassDcmpSpkesFile:
+    :return:
+    """
+
+    if isinstance(pMassFile,str):
+        if os.path.isfile(pMassFile):
+            # Load in the data to see what the hell it is
+            inputDataFile = open(pMassFile, "rb")
+            dataOut = pickle.load(inputDataFile)
+            inputDataFile.close()
+            # Now we need to write a description of what the data is! [dataOut, xmin, xmax, vmin, vmax, amin, amax, dt, tmax]
+            numbOfTrials = len(dataOut[0]) # This is the number of individual trails stored
+            xmin = dataOut[1]
+            xmax = dataOut[2]
+            vmin = dataOut[3]
+            vmax = dataOut[4]
+            amin = dataOut[5]
+            amax = dataOut[6]
+            dt = dataOut[7]
+            tmax = dataOut[8]
+            descpFName = pMassFile[:-4]+'DESC.txt'
+            file = open(descpFName,'w')
+            file.write("1-D Moving Mass Data Description\n")
+            file.write("\nData here is generated from the genAndSaveMoving1DMassData() method\n")
+            file.write("\nFileName... "+pMassFile)
+            file.write("\niterations. "+str(numbOfTrials))
+            file.write("\nxmin....... "+str(xmin))
+            file.write("\nxmax....... "+str(xmax))
+            file.write("\nvmin........"+str(vmin))
+            file.write("\nvmax........"+str(vmax))
+            file.write("\namin........"+str(amin))
+            file.write("\namax........"+str(amax))
+            file.write("\ndt.........."+str(dt))
+            file.write("\ntmax........"+str(tmax))
+            file.close
+    if isinstance(pMassDcmpFile, str):
+        if os.path.isfile(pMassDcmpFile):
+            # Load in teh data to see what it has it there
+            inputDataFile = open(pMassDcmpFile, "rb")
+            dataOut = pickle.load(inputDataFile)
+            inputDataFile.close()
+            # Now we need to write a description of what the data is! [segmentedTrials, gCenters, b, dataFile, xStart, xStop, nGaus, bInitial, dt]
+            numbOfTrials = len(dataOut[0])
+            originatingDataFile = dataOut[3]
+            numbGaus = dataOut[6]
+            xStart = dataOut[4]
+            xStop = dataOut[5]
+            GausVarb = dataOut[2]
+            dt = dataOut[8]
+            descpFName = pMassDcmpFile[:-4]+"DESC.txt"
+            file = open(descpFName,'w')
+            file.write("1-D Moving Mass Decomposed Data Description\n")
+            file.write("\nData here is generated from the decomposeMoving1DMassData() method\n")
+            file.write("\nFileName......."+pMassDcmpFile)
+            file.write("\nInputDataFile.."+originatingDataFile)
+            file.write("\niterations....."+str(numbOfTrials))
+            file.write("\nNumbOfGauss...."+str(numbGaus))
+            file.write("\nxStart........."+str(xStart))
+            file.write("\nxStop.........."+str(xStop))
+            file.write("\nGausVarbl_b...."+str(GausVarb))
+            file.write("\ndt............."+str(dt))
+            file.close()
+    if isinstance(pMassDcmpSpkesFile, str):
+        if os.path.isfile(pMassDcmpSpkesFile):
+            # Load in teh data to see what it has it there
+            inputDataFile = open(pMassDcmpSpkesFile, "rb")
+            dataOut = pickle.load(inputDataFile)
+            inputDataFile.close()
+            # Now we need to write a description of what the data is! [segmentedSpikesList, dataFile, spikeGenType, analgSingnalScaling]
+            numbOfTrials = len(dataOut[0])
+            originatingDataFile = dataOut[1]
+            spikeGenType = dataOut[2]
+            analogScl = dataOut[3]
+            descpFName = pMassDcmpSpkesFile[:-4]+"DESC.txt"
+            file = open(descpFName,'w')
+            file.write("1-D Moving mass Decomposed and Turned to Spikes Data Description\n")
+            file.write("\nData here is generated from the decompedToSpikes1DMassData\n")
+            file.write("\nFileName........."+pMassDcmpSpkesFile)
+            file.write("\nInputDataFile...."+originatingDataFile)
+            file.write("\nIterations......."+str(numbOfTrials))
+            file.write("\nSpike-Gen-Type..."+spikeGenType)
+            file.write("\nSignal-Scaling..."+str(analogScl))
+            file.close()
+
+
+
+
+
+
 ########################################################################################################################
 # RUNNING METHODS
 ########################################################################################################################
@@ -318,7 +414,7 @@ if __name__ == "__main__":
     # TESTING METHODS
     ####################################################################################################################
 
-    print('Begin testing ... ... \n\n')
+    # print('Begin testing ... ... \n\n')
 
     # CREATE AND SAVE SOME 1D MOVING MASS DATA
     #genAndSaveMoving1DMassData()
@@ -328,7 +424,7 @@ if __name__ == "__main__":
     #loadAndPlot1DMassData()
 
     # DECOMPOSE 1D MOVING MASS DATA
-    # decomposeMoving1DMassData()
+    #decomposeMoving1DMassData()
 
     # PLOT THE 1D MOVING MASS DATA AND THE DECOMPOSITION OF SAID DATA
     #loadAndPlotDecomp1DMassData()
@@ -337,4 +433,26 @@ if __name__ == "__main__":
     #decompedToSpikes1DMassData()
 
     # PLOT THE SPIKED VERSIONOF THE DECOMPOSED DATA FOR ACCURACY OF THE APPROACH
-    loadAndPlotDecomp1DMassData()
+    #loadAndPlotDecomp1DMassData()
+
+    # CREATE TXT FILE DESCRIPTIONS
+    createDataDescriptionTxtFile(pMassFile='movingPointMassData/pointMassData000.pkl',pMassDcmpFile='movingPointMassData/pointMassDataDecmp000.pkl',pMassDcmpSpkesFile='movingPointMassData/pointMassDataDecmpSpikes000.pkl' )
+
+    ####################################################################################################################
+    # GENERATE EXPERIMENTAL DATA
+    ####################################################################################################################
+    # Here we actually will generate the data used in the experiments, we will need quite a bit of data. Note when
+    # saving data the method zfill() seems pretty handy!
+
+    # GENERATE 1D POINT MASS DATA
+    #numberOfTrials = 10
+    #topDirectory = 'movingPointMassData/'
+    #subDirectory = 'dataSet_00/'
+    #pMassDataName = 'pMassData'
+    #pMassDcmpDataName = 'pMassDataDcmp'
+    #pMassDcmpSpksDataName = 'pMassDcmpSpksData'
+    #fileType = '.pkl'
+    #First Step is to make the 1D Point Mass Data!
+    #saveName = topDirectory+subDirectory+pMassDataName+fileType
+
+
